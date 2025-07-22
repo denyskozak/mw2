fdd0b1ebc78e85ed708b91de5e338f25881e4871
denyskozak <denys.kozak.self@gmail.com>
2025-07-22 13:17:37 +0100

    init

---
+++ public/models/README.md
@@ -0,0 +1,2 @@
+This directory stores local 3D models for the game.
+Place `zone-fantasy-1.glb` here to load the world model locally.
---
+++ src/consts/models.js
@@ -0,0 +1,7 @@
+export const models = {
+  brand: {
+    name: 'Brand',
+    path: '/models/skins/brand.glb',
+    scale: 0.01
+  }
+}
